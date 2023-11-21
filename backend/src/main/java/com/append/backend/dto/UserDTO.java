a8b7f56324ed1282500beb7b1779c549fb97c76a
Marco Túlio Amaral <docs.mtamaral@gmail.com>
2023-11-20 22:12:23 -0300

    Spring project created

---
+++ backend/src/main/java/com/append/backend/dto/UserDTO.java
@@ -0,0 +1,38 @@
+package com.append.backend.dto;
+
+import com.append.backend.entities.User;
+
+public class UserDTO {
+    private long id;
+    private String name;
+
+    public UserDTO(){}
+
+    public UserDTO(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UserDTO(User entity){
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
---
+++ backend/src/main/java/com/append/backend/entities/Role.java
@@ -0,0 +1,5 @@
+package com.append.backend.entities;
+
+public class Role {
+
+}
