78cd14ed1c1d25217a97643bfb4c26d2e1a0603d
lucaslopesxx <lucasalvarengalopes@gmail.com>
2023-11-23 14:58:16 -0300

    Login Page Implemented

---
+++ Sistema/frontend/src/components/header/header.tsx
@@ -0,0 +1,19 @@
+import '../header/header.css'
+import logo from '../../assets/whiteLogo.svg'
+import menu from '../../assets/iconeMenu.svg'
+import { Link } from 'react-router-dom'
+
+function header() {
+    return (
+        <section className="header-background">
+            <section className='header-content'>
+                <Link to={'/app'} className='logo-link'>
+                    <img src={logo} className="header-logo" alt="logo" />
+                </Link>
+                <img src={menu} className='header-menu-icon' alt='menu icon'/>
+            </section>
+        </section>
+    )
+}
+
+export default header
--- Sistema/frontend/src/pages/App.tsx
+++ Sistema/frontend/src/pages/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
+import logo from '../assets/orangeLogo.svg';
 import '../styles/App.css';
 
 function App() {
