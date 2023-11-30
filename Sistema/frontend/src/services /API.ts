9e02d5b283656d52a684015b39c96065bf50d29a
Lucas Lopes <104603745+lucaslopesxx@users.noreply.github.com>
2023-11-30 19:52:01 -0300

    Git conflict delete

--- Sistema/frontend/src/services /API.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    //baseURL: "https://cors-anywhere.herokuapp.com/http://localhost:8081/",
-    baseURL: "http://localhost:8080/",
-});
-
-export default api;
